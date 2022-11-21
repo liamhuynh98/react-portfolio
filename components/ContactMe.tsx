f237e6ab80dc4127482f9b094c704534ee8fd04c
Liam Huynh <liam@crowdhubapps.com>
2022-11-21 15:09:50 -0600

    will need to setup email server, scrollbar redesign

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    name: string;
+    email: string;
+    subject: string;
+    message: string;
+};
 
 type Props = {}
 
 const ContactMe = (props: Props) => {
+
+    const { register, handleSubmit } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (formData) => {
+        window.location.href=`mailto:liamhuynh21@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+    };
+
   return (
     <div className='h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
@@ -13,10 +28,40 @@ const ContactMe = (props: Props) => {
                  I have what you need. { " " }
                  <span className='decoration-[#F7AB0A]/50 underline'> Let's Talk</span>
             </h4>
-            <div></div>
+
+            <div className='space-y-10'>
+                <div className='flex items-center space-x-5 justify-center'>
+                    <PhoneIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
+                    <p className='text-2xl'>12341233452</p>
+                </div>
+
+                <div className='flex items-center space-x-5 justify-center'>
+                    <EnvelopeIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
+                    <p className='text-2xl'>liamhuynh21@gmail.com</p>
+                </div>
+
+                <div className='flex items-center space-x-5 justify-center'>
+                    <MapPinIcon className='text-[#F7AB0A] h-7 w-7 animate-pulse'/>
+                    <p className='text-2xl'>123 Developer Adress</p>
+                </div>
+            </div>
+
+            <form onSubmit={handleSubmit(onSubmit)}  className='flex flex-col space-y-2 w-fit mx-auto'>
+
+                <div className='flex space-x-2'>
+                     <input {...register('name')} placeholder='Name' className='contactInput' type='text'></input>
+                     <input {...register('email')}placeholder='Email' className='contactInput' type='email'></input>
+                </div>
+
+                <input {...register('subject')} placeholder='Subject' className='contactInput' type='text'></input>
+
+                <textarea {...register('message')} placeholder='Message' className='contactInput'></textarea>
+
+                <button type='submit' className='bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold'>Submit</button>
+            </form>
         </div>
     </div>
   )
 }
 
-export default ContactMe
+export default ContactMe 
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
     <Particle/>
-    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-1' >
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-1' >
         <Head>
           <title>Liam's Portfolio</title>
         </Head>
